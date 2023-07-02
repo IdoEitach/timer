39832c80b2035691b18dacc11e9feeaacb3c2ed3
IdoEitach <112800537+IdoEitach@users.noreply.github.com>
2023-07-02 23:23:25 +0300

    change from class to new func method React

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import Timer from './timer'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import Timer from "./timer";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
-root.render(<Timer
-  seconds={30} 
-  size={120}
-  strokeBgColor="black"
-  strokeColor="red"
-  textColor="blue"
-  strokeWidth={3}
-  opacityAfterClick={0.5}
+root.render(
+  <Timer
+    seconds={30}
+    strokeColor="red"
+    strokeWidth={3}
+    renderTms={10}
+    isPlaying={true}
   />
 );
-
